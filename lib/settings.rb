b9cb10ab38503c31d4df91dd02e1bbe1f3d0e57d
Tomislav Car <tomislav@infinum.hr>
2011-07-11 14:08:09 +0200

    Added setup to autorecognize .xcodeproj name, added copying of .ipa, .mobileprovision and .plist files from deployed folder. Fixed bug with trailing slash on url

--- lib/settings.rb
+++ lib/settings.rb
@@ -1,19 +1,21 @@
 class Settings
-  attr_accessor :name, :url, :path, :filename, :plist_file, :provision_file_name, :remote_user, :host, :type, :template_file
+  attr_accessor :name, :url, :path, :filename, :plist_file, :provision_file_name, :remote_user, :host, :type, :template_file, :ipa_file
   
   def initialize(options)
     @name = options[:name]
     @url = options[:url]
+    @url+= "/" unless @url.match(/\/$/)
     @host = options[:host]
     @path = options[:path]
     @type = options[:type]
     @remote_user = options[:remote_user]
 
-    @filename = @name.downcase.gsub(" ", "_")
-    @provision_file_name = "#{@filename}_AdHoc.mobileprovision"
+    @filename = @name.gsub(" ", "_")
+    @provision_file_name = "#{@filename}.mobileprovision"
     @plist_file = "#{@filename}.plist"
+    @ipa_file = "#{@filename}.ipa"
     @plist_file_url = "#{@url}#{@plist_file}"
-    @template_file = "#{COMBAT_ROOT}/templates/template_#{@type}.erb"
+    @template_file = options[:template] || "#{COMBAT_ROOT}/templates/template_#{@type}.erb"
     @qrcode = "http://qrcode.kaywa.com/img.php?s=8&d=#{ERB::Util.url_encode(@url)}"
   end
   
